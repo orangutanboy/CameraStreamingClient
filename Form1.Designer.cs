3cc3c356353728586adf928cf5333526b23a21a6
M_J_O_N_E_S <M_J_O_N_E_S@hotmail.com>
2013-07-02 22:15:31 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace BluetoothWindowsClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 2000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(86, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 283);
+            this.progressBar1.Maximum = 230454;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(470, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 377);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BluetoothWindowsClient
+{
+    public partial class Form1 : Form
+    {
+        BluetoothClient _bluetoothClient = new BluetoothClient();
+        NetworkStream _bluetoothStream;
+
+        byte[] _allBytes = new byte[300000];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void ConnectBluetooth()
+        {
+            await Task.Run(() =>
+            {
+                try
+                {
+                    var gadgeteerDevice = _bluetoothClient.DiscoverDevices()
+                                                   .Where(d => d.DeviceName == "Gadgeteer")
+                                                   .FirstOrDefault();
+
+                    if (gadgeteerDevice != null)
+                    {
+                        _bluetoothClient.SetPin("0000");
+                        _bluetoothClient.Connect(gadgeteerDevice.DeviceAddress, BluetoothService.SerialPort);
+                        _bluetoothStream = _bluetoothClient.GetStream();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.Print(ex.Message);
+                }
+            });
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (!_bluetoothClient.Connected)
+            {
+                ConnectBluetooth();
+            }
+
+            if (_bluetoothStream == null)
+            {
+                return;
+            }
+
+            using (Stream stream = ReadToSize(_bluetoothStream, 230454))
+            {
+                if (stream.Length > 0)
+                {
+                    DrawFromStream(stream);
+                    timer.Enabled = false;
+                }
+            }
+        }
+
+        private void DrawFromStream(Stream stream)
+        {
+            var bitmap = Bitmap.FromStream(stream);
+            pictureBox1.Image = bitmap;
+        }
+
+        public Stream ReadToSize(NetworkStream stream, int size)
+        {
+            byte[] buffer = new byte[size];
+            var ms = new MemoryStream();
+            while (ms.Length < size)
+            {
+                int read = stream.Read(buffer, 0, buffer.Length);
+
+                if (read > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                    progressBar1.Value = (int)ms.Length;                   
+                }
+            }
+            return ms;
+        }
+
+
+        private void WriteIt(byte[] bytes, int length)
+        {
+            using (var fileStream = File.OpenWrite(@"c:\temp\gadgeteer.bmp"))
+            {
+                fileStream.Write(bytes, 0, length);
+            }
+        }
+    }
+}
