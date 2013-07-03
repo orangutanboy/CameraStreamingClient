632ea1170cf865ff1485806839419edf0190ea99
M_J_O_N_E_S <M_J_O_N_E_S@hotmail.com>
2013-07-03 20:22:55 +0100

    Tidied up for public consumption

--- Form1.cs
+++ Form1.cs
@@ -16,15 +16,15 @@ namespace BluetoothWindowsClient
         BluetoothClient _bluetoothClient = new BluetoothClient();
         NetworkStream _bluetoothStream;
 
-        byte[] _allBytes = new byte[300000];
-
         public Form1()
         {
             InitializeComponent();
         }
 
-        private async void ConnectBluetooth()
+        private async void ConnectBluetoothAsync()
         {
+            //Attempt to connect to a known device, with a known pin.
+            //Don't block the UI thread.
             await Task.Run(() =>
             {
                 try
@@ -49,56 +49,61 @@ namespace BluetoothWindowsClient
 
         private void timer_Tick(object sender, EventArgs e)
         {
+            //Attempt to connect to the bluetooth device
             if (!_bluetoothClient.Connected)
             {
-                ConnectBluetooth();
+                ConnectBluetoothAsync();
             }
 
+            //Only continue if the NetworkSteam is defined (this is a property of the bluetooth client)
             if (_bluetoothStream == null)
             {
                 return;
             }
 
+            //Read the next 230454 bytes received (230454 is the size of a 320x240 bitmap)
             using (Stream stream = ReadToSize(_bluetoothStream, 230454))
             {
+                //If data was received
                 if (stream.Length > 0)
                 {
-                    DrawFromStream(stream);
+                    //Stop the timer, it's not needed any more
                     timer.Enabled = false;
+
+                    //Convert the bytes into an image and display
+                    DrawFromStream(stream);
                 }
             }
         }
 
         private void DrawFromStream(Stream stream)
         {
+            // Reconstitute the bytes in the stream into a bitmap and show it
             var bitmap = Bitmap.FromStream(stream);
             pictureBox1.Image = bitmap;
         }
 
+        //Reads the next [size] bytes from the stream
         public Stream ReadToSize(NetworkStream stream, int size)
         {
             byte[] buffer = new byte[size];
-            var ms = new MemoryStream();
-            while (ms.Length < size)
+            var memoryStream = new MemoryStream();
+            
+            //Keep going until all the expeected bytes are received
+            while (memoryStream.Length < size)
             {
-                int read = stream.Read(buffer, 0, buffer.Length);
+                //Read the next chunk
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
 
-                if (read > 0)
+                //If any were read, write them out to the stream
+                if (bytesRead > 0)
                 {
-                    ms.Write(buffer, 0, read);
-                    progressBar1.Value = (int)ms.Length;                   
+                    memoryStream.Write(buffer, 0, bytesRead);
+                    //Feed back current progress
+                    progressBar1.Value = (int)memoryStream.Length;
                 }
             }
-            return ms;
-        }
-
-
-        private void WriteIt(byte[] bytes, int length)
-        {
-            using (var fileStream = File.OpenWrite(@"c:\temp\gadgeteer.bmp"))
-            {
-                fileStream.Write(bytes, 0, length);
-            }
+            return memoryStream;
         }
     }
 }
